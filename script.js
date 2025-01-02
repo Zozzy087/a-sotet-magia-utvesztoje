d250bf06b1a8979671a230d067a8b03bc2142e7b
Zozzy087 <lughlleu@gmail.com>
2025-01-02 22:03:15 +0100

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function checkCode() {
         result.className = "success";
         // Itt átirányíthatod a játékost a következő fejezetre vagy megjelenítheted a folytatást
     } else {
-        result.innerHTML = "Helytelen kód. Próbáld újra!";
+        result.innerHTML = "Helytelen válasz. Kérlek próbáld újra!";
         result.className = "error";
     }
 }
