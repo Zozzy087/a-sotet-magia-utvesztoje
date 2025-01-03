678d4d7862abb4084ea92c6c4ab24aaddabf8963
Zozzy087 <lughlleu@gmail.com>
2025-01-03 15:13:16 +0100

    script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function checkCode() {
     
     // Itt állítsd be a helyes kódot
     if (code === "25") {
-        result.innerHTML = "Helyes! Lapozz a [78]-ra a következő fejezethez!";
+        result.innerHTML = "Helyes! Lapozz a [63]-ra a következő fejezethez!";
         result.className = "success";
         // Itt átirányíthatod a játékost a következő fejezetre vagy megjelenítheted a folytatást
     } else {
