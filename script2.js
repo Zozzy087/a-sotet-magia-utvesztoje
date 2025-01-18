f3a6cdea4901b88eaecb7edfdb1a1375dc064159
Zozzy087 <lughlleu@gmail.com>
2025-01-18 15:58:06 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,14 @@
-
+function checkCode() {
+    const code = document.getElementById('code').value;
+    const result = document.getElementById('result');
+    
+    // Itt állítsd be a helyes kódot
+    if (code === "6") {
+        result.innerHTML = "Helyes! Lapozz a [113]-ra a következő fejezethez!";
+        result.className = "success";
+        // Itt átirányíthatod a játékost a következő fejezetre vagy megjelenítheted a folytatást
+    } else {
+        result.innerHTML = "Helytelen válasz. Veszítesz 3 ÉP! Próbáld újra!";
+        result.className = "error";
+    }
+}
